3b93b3e0bb6b44ae05dc02292faa9d16bf805f1d
ranjansaga <ranjanprabhub@gmail.com>
2025-04-28 13:57:04 +0530

    Update separateBlackWhiteBalls.js

--- separateBlackWhiteBalls.js
+++ separateBlackWhiteBalls.js
@@ -13,7 +13,8 @@
 // Explanation: We can group all the black balls to the right in the following way:
 // - Swap s[0] and s[1], s = "011".
 // Initially, 1s are not grouped together, requiring at least 1 step to group them to the right.
-// Example 2:
+
+
 var minimumSteps = function(s) {
     let zeroCount = 0;  // number of zeros encountered so far
     let steps = 0;  // total number of swaps
